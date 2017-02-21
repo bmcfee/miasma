1b094ecc762d4adb1a17949b3c721e952f6960ab
Justin Salamon <justin.salamon@gmail.com>
2017-02-20 19:41:22 -0500

    Add version printouts

--- miasma/__init__.py
+++ miasma/__init__.py
@@ -1,1 +1,12 @@
 # CREATED: 2/20/17 19:02 by Justin Salamon <justin.salamon@nyu.edu>
+
+import keras
+import theano
+import numpy
+import pescador
+
+print('keras version: {:s}'.format(keras.__version__))
+print('theano version: {:s}'.format(theano.__version__))
+print('pescador version: {:s}'.format(pescador.__version__))
+print('numpy version: {:s}'.format(numpy.__version__))
+
