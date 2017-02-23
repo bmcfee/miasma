96a2820a526d63938cdb16f8fc70e8180b3364ae
Justin Salamon <justin.salamon@gmail.com>
2017-02-23 18:16:27 -0500

    adjust

--- miasma/eval_exp.py
+++ miasma/eval_exp.py
@@ -66,8 +66,8 @@ def eval_exp(expid):
         avg_baseline = split_results[:, 5].mean()
 
         report = ('averag: acc: {:.2f}\tpre: {:.2f}\trec: {:.2f}' +
-                  '\t0/1: {:.0f}/{:.1f}\tbaseline: {:.2f}').format(
+                  '\t0/1: {:.0f}/{:.0f}\tbaseline: {:.2f}').format(
             avg_acc, avg_precision, avg_recall, avg_zeros, avg_ones,
             avg_baseline)
-        print(colored(report, 'red'))
+        print(colored(report, 'magenta'))
 
