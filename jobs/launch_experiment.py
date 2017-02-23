81de0184ec9161833d12f07e06c5aa584da1f524
Justin Salamon <justin.salamon@gmail.com>
2017-02-22 19:31:29 -0500

    Template qfile and launch script

---
+++ jobs/launch_experiment.py
@@ -0,0 +1,74 @@
+# CREATED: 2/22/17 16:52 by Justin Salamon <justin.salamon@nyu.edu>
+
+import argparse
+import os
+import shutil
+import subprocess
+from miasma.vad_mil import run_experiment
+
+
+def launch_experiment(expid):
+
+    # Create folder for experiment jobs
+    expfolder = os.path.expanduser(os.path.join('~/dev/miasma/jobs', expid))
+    if not os.path.isdir(expfolder):
+        os.mkdir(expfolder)
+
+    # Load in template
+    qfile = os.path.expanduser('~/dev/miasma/jobs/milexp.q')
+    with open(qfile, 'r') as file:
+        data = file.readlines()
+        file.close()
+
+    # Get the last line (the one we care about)
+    d = data[-1].split(' ')
+
+    for pooling_layer in ['softmax', 'max', 'mean', 'none']:
+        for split_idx in [2, 3, 4, 5, 6]:
+
+            # Copy qsub template
+            destfile = os.path.expanduser(
+                os.path.join(expfolder, 'milexp_{:s}{:d}.q'.format(
+                    pooling_layer, split_idx)))
+            shutil.copyfile(qfile, destfile)
+
+            data_fold = data[:]
+
+            assert d[-4] == '--split_indices'
+            d[3] = str(split_idx)
+
+            assert d[-2] == '--pool_layers'
+            d[-1] = pooling_layer
+
+            data_fold[-1] = ' '.join(d)
+
+            assert data_fold[4].startswith('#PBS -N ')
+            data_fold[4] = '#PBS -N mil{:d}_{:s}_{:s}\n'.format(
+                int(expid[3:]), pooling_layer, split_idx)
+
+            # add a couple of empty lines at the end
+            data_fold.append('\n')
+            data_fold.append('\n')
+
+            with open(destfile, 'w') as file:
+                file.writelines(data_fold)
+
+            print(' ')
+            print('Launching %s'.format(os.path.basename(destfile)))
+            print(data_fold[-3])
+
+            jobid = subprocess.check_output('qsub {:s}'.format(destfile),
+                                            shell=True)
+            jobid = jobid.strip('\n')
+            print('jobid = {:s} ({:d})'.format(jobid, len(jobid)))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('expid', type=str)
+
+    args = parser.parse_args()
+
+    launch_experiment(args.expid)
+
---
+++ jobs/milexp.q
@@ -0,0 +1,25 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1:gpus=1
+#PBS -l walltime=03:00:00
+#PBS -l mem=16gb
+#PBS -N mil
+#PBS -M justin.salamon@gmail.com
+#PBS -m abe
+#PBS -j oe
+#PBS -p 0
+
+module purge
+
+SRCDIR=$HOME/dev/miasma
+
+RUNDIR=$SCRATCH/jobs/miasma/run-${PBS_JOBID/.*}
+mkdir $RUNDIR
+cd $RUNDIR
+
+module load cuda/7.5.18
+module load cudnn/7.5v5.1
+module load sox/intel/14.4.1
+
+source activate py35hpcT9b
+
+THEANO_FLAGS="base_compiledir=$PBS_JOBTMP,device=gpu,floatX=float32,mode=FAST_RUN" python $SRCDIR/jobs/launch_experiment.py exp003 --n_bag_frames 44 --min_active_frames 10 --act_threshold 0.5 --n_hop_frames 22 --batch_size 32 --n_active 1000 --samples_per_epoch 1024 --nb_epochs 50 --verbose 1 --tf_rows 288 --tf_cols 44 --nb_filters 32 32 --kernel_sizes 3 3 3 3 --nb_fullheight_filters 32 --loss binary_crossentropy --optimizer adam --metrics accuracy precision recall --split_indices 2 --pool_layers softmax
